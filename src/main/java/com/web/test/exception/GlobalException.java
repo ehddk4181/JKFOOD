315520481a1efe47da196f84a190d83a301c5844
ehddk4181 <1990wornjs@gmail.com>
2019-10-11 14:33:56 +0900

    JK_FOOD_Push

--- src/main/java/com/web/test/exception/GlobalException.java
+++ src/main/java/com/web/test/exception/GlobalException.java
@@ -6,16 +6,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalException {
-	@ExceptionHandler(NullPointerException.class)
-	public String nullpointHandler(Model model , NullPointerException ex){
-		model.addAttribute("Message", "Null 에러 발생");
-		model.addAttribute("ex", ex);
-		return "/Exception/error";
-	}
+
 	@ExceptionHandler(Exception.class)
 	public String exceptionHandler(Model model , Exception ex){
-		model.addAttribute("Message", "기타 에러 발생");
-		model.addAttribute("ex", ex);
-		return "/Exception/error";
+		return "/error";
 	}
 }
